c99ef2b4fb1b41a8ed467b69382843b20255204e
Steve Zehngut <steve@zeek.com>
2015-02-19 09:26:04 -0800

    Added foundation to index.php and front-page.php

--- front-page.php
+++ front-page.php
@@ -9,34 +9,47 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="row"><!-- .row start -->
 
-		<?php if ( have_posts() ) : ?>
+	<div class="large-8 columns"><!-- .columns start -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'page-templates/partials/content', get_post_format() );
-				?>
+			<?php if ( have_posts() ) : ?>
 
-			<?php endwhile; ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php the_posts_navigation(); ?>
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'page-templates/partials/content', get_post_format() );
+					?>
 
-		<?php else : ?>
+				<?php endwhile; ?>
 
-			<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
+				<?php the_posts_navigation(); ?>
 
-		<?php endif; ?>
+			<?php else : ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
 
-<?php get_sidebar(); ?>
+			<?php endif; ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- .columns end -->
+
+	<div class="large-4 columns"><!-- .columns start -->
+
+		<?php get_sidebar(); ?>
+
+	</div><!-- .columns end -->
+
+</div><!-- .row end -->
+
 <?php get_footer(); ?>

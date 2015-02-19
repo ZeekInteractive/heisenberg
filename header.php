d324cf7107f5dfa7bd63d52c7b6a1d18a1adcbc6
Steve Zehngut <steve@zeek.com>
2015-02-19 10:30:36 -0800

    Added foundation rows and columns to header.

--- header.php
+++ header.php
@@ -21,14 +21,43 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<div class="row"><!-- .row start -->
+
+			<div class="small-12 columns"><!-- .columns start -->
+
+				<div class="site-branding">
+
+					<div class="row"><!-- .row start -->
+
+						<div class="large-6 small-12 columns"><!-- .columns start -->
+
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+						</div><!-- .columns end -->
+
+						<div class="large-6 small-12 columns"><!-- .columns start -->
+
+							<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+
+						</div><!-- .columns end -->
+
+					</div><!-- .row end -->
+
+				</div><!-- .site-branding -->
+
+			</div><!-- .columns end -->
+
+			<div class="small-12 columns"><!-- .columns start -->
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+				</nav><!-- #site-navigation -->
+
+			</div><!-- .columns end -->
+
+		</div><!-- .row end -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content row">

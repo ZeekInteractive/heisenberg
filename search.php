a98e010e1cb92d9ba9cc03f33dfa55b46889788d
Jacob Arriola <jacob.arriola@gmail.com>
2015-08-11 20:22:14 -0700

    Add missing row and columns elements to search.php template The search template was missing its row and column elements, which broke the template styles in relation to the rest of the project

--- search.php
+++ search.php
@@ -7,39 +7,45 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section class="row"><!-- .row start -->
 
-		<?php if ( have_posts() ) : ?>
+	<section class="columns"><!-- .columns start -->
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'heisenberg' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+		<section id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) : ?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'page-templates/partials/content', 'search' );
-				?>
+				<header class="page-header">
+					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'heisenberg' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				</header><!-- .page-header -->
 
-			<?php endwhile; ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php the_posts_navigation(); ?>
+					<?php
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'page-templates/partials/content', 'search' );
+					?>
 
-		<?php else : ?>
+				<?php endwhile; ?>
 
-			<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
+				<?php the_posts_navigation(); ?>
 
-		<?php endif; ?>
+			<?php else : ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+				<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
 
+			<?php endif; ?>
+
+			</main><!-- #main -->
+		</section><!-- #primary -->
+
+	</section><!-- columns -->
+</section><!-- row -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>

90aeadee62d6f402805883eddee3c4d36c012abf
Jacob Arriola <jacob@zeek.com>
2015-11-21 15:32:31 -0800

    :fire: Move JS folder into our gulp assets
    Also removing unused items.

--- assets/js/sample.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-
-  // This is a sample jQuery file.  Create as many of these as you like. Gulp
-  // will grab all files here and concat them to ./assets/dist/js/app.js &
-  // ./assets/dist/js/app.min.js.  This theme is set to enqueue both files
-  // depending on whether you've set define(WP_DEBUG, 'true'); in your
-  // wp-config.php file.
-
-});

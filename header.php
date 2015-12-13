54e89bcea5a0101b3f14ad32aa2da8bfb7810992
Jacob Arriola <jacob@zeek.com>
2015-12-13 11:41:25 -0800

    🎨 Add basic offcanvas styles
    Also clean up markup spacing a bit.

--- header.php
+++ header.php
@@ -19,46 +19,44 @@
 
 <body <?php body_class(); ?>>
 
+<div class="off-canvas-wrapper">
+  <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+    <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas data-position="left">
+			<button class="close-button" aria-label="Close alert" type="button" data-toggle="offCanvasLeft">
+				<span aria-hidden="true">&times;</span>
+			</button>
+			<a href="<?php bloginfo('url'); ?>" class="ofc-home">Home</a>
+			<?php
+			 $args = array (
+				 'theme_location' 	=> 'primary',
+				 'container' 				=> 'nav',
+				 'container_class'	=> 'offcanvas-navigation',
+				 'menu_class' 			=> 'mobile-menu',
+			 );
+				wp_nav_menu( $args );
+			?>
+    </div><!-- #offCanvasLeft -->
+    <div class="off-canvas-content" data-off-canvas-content>
+			<div class="title-bar show-for-small-only">
+			  <div class="title-bar-left">
+			    <button class="menu-icon" type="button" data-open="offCanvasLeft"></button>
+			    <span class="title-bar-title"><?php bloginfo( 'name' ); ?></span>
+			  </div>
+			</div><!-- .title-bar -->
+			<header id="masthead" class="" role="banner">
+				<section class="row column">
+					<h1 class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<?php bloginfo( 'name' ); ?>
+						</a>
+					</h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</section>
+				<nav id="site-navigation" class="top-bar show-for-medium" data-topbar role="navigation">
+					<section class="top-bar-section row column">
+						<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+					</section>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
 
-	<div class="off-canvas-wrapper">
-	    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-	      <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas data-position="left">
-					<button type="button" class="button" data-toggle="offCanvasLeft">X</button>
-					<a href="<?php bloginfo('url'); ?>">Home</a>
-					<?php
-					 $args = array (
-						 'theme_location' 	=> 'primary',
-						 'container' 				=> 'nav',
-						 'container_class'	=> 'offcanvas-navigation',
-						 'menu_class' 			=> 'mobile-menu',
-					 );
-						wp_nav_menu( $args );
-					?>
-	      </div><!-- #offCanvasLeft -->
-	      <div class="off-canvas-content" data-off-canvas-content>
-
-				<div class="title-bar show-for-small-only">
-				  <div class="title-bar-left">
-				    <button class="menu-icon" type="button" data-open="offCanvasLeft"></button>
-				    <span class="title-bar-title"><?php bloginfo( 'name' ); ?></span>
-				  </div>
-				</div>
-
-
-	<header id="masthead" class="" role="banner">
-		<section class="row column">
-			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
-				</a>
-			</h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</section>
-		<nav id="site-navigation" class="top-bar show-for-medium" data-topbar role="navigation">
-			<section class="top-bar-section row column">
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</section>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+			<div id="content" class="site-content">

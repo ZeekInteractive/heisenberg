3463a6eeb0db4f4c58bd94303e4c82489977f965
Jacob Arriola <jacob@zeek.com>
2015-12-13 11:39:44 -0800

    🎨 Remove excess markup

--- footer.php
+++ footer.php
@@ -8,25 +8,17 @@
  */
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+			<footer id="colophon" class="site-footer" role="contentinfo">
 
-		<div class="row"><!-- .row start -->
+				<div class="column row"><!-- .column.row start -->
 
-			<div class="small-12 columns"><!-- .columns start -->
-
-				<div class="site-info">
 					<p class="text-center">This is the footer</p>
-				</div><!-- .site-info -->
 
-			</div><!-- .columns end -->
+				</div><!-- .column.row end -->
 
-		</div><!-- .row end -->
-
-	</footer><!-- #colophon -->
-
-
+			</footer><!-- #colophon -->
 
 		</div> <!-- .off-canvas-content -->
 	</div><!-- .off-canvas-wrapper-inner -->

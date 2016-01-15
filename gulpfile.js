c16f1303ae38943192be4af733c0c71497057847
Jacob Arriola <jacobarriola@users.noreply.github.com>
2016-01-15 08:16:10 -0800

    Remove js-lint require function
    We had removed the task a while back, but the require function still existed in our variables.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,14 @@ var	gulp			= require('gulp'),
 	sass			= require('gulp-sass'),  // Our sass compiler
 	notify			= require('gulp-notify'), // Basic gulp notificatin using OS
 	sourcemaps		= require('gulp-sourcemaps'), // Sass sourcemaps
-	rename			= require('gulp-rename'), // Allows us to rename our css file prior to minifying
+	rename			= require('gulp-rename'), // Allows us to rename our JS file prior to minifying
 	autoprefixer		= require('gulp-autoprefixer'), // Adds vendor prefixes for us
-	browserSync		= require('browser-sync'), // Sends php, js, img and css updates to browser for us
+	browserSync		= require('browser-sync'), // Sends php, js, and css updates to browser for us
 	concat			= require('gulp-concat'), // Concat our js
-	uglify			= require('gulp-uglify'), // Minify our js
-	jshint 			= require('gulp-jshint'); // Checks for js errors
+	uglify			= require('gulp-uglify'); // Minify our js
+
 
 // Our browser-sync task.
-
 gulp.task('browser-sync', function() {
 	var files = [
 		'**/*.php'
@@ -24,7 +23,6 @@ gulp.task('browser-sync', function() {
 
 
 // Our 'styles' tasks, which handles our sass actions such as compliling and minification
-
 gulp.task('styles', function() {
 	gulp.src('assets/sass/**/*.scss')
 		.pipe(sourcemaps.init())
@@ -37,7 +35,7 @@ gulp.task('styles', function() {
 		)
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions', 'ie >= 9']
-		})) // our autoprefixer - add and remove vendor prefixes using caniuse.com
+		})) 
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./assets/dist/css')) // Location of our app.css file
 		.pipe(browserSync.stream({match: '**/*.css'}))

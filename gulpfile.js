8f12584f883c815716ed9033626167f1a6988b9d
Aaron Holbrook <aaron@zeek.com>
2016-08-10 11:40:19 -0700

    Change default gulp command

--- gulpfile.js
+++ gulpfile.js
@@ -212,4 +212,5 @@ gulp.task('watch', function() {
 
 
 // Our default gulp task, which runs all of our tasks upon typing in 'gulp' in Terminal
-gulp.task('default', ['svg-sprite', 'styles', 'js', 'browser-sync', 'foundation-js', 'watch']);
+gulp.task('default', ['styles', 'js', 'svg-sprite']);
+gulp.task('serve', ['svg-sprite', 'styles', 'js', 'browser-sync', 'foundation-js', 'watch']);

57045f1f2d13570e10b6021f3abe6472afa45b3b
Jacob Arriola <jacobarriola@users.noreply.github.com>
2016-11-29 08:34:34 -0800

    Change autoload path

--- functions.php
+++ functions.php
@@ -5,10 +5,10 @@
 
 namespace Heisenberg;
 
-define( 'HEISENBERG_VERSION', '2.0.3' );
+define( 'HEISENBERG_VERSION', '2.0.4' );
 define( 'HEISENBERG_DIR', __DIR__ );
 define( 'HEISENBERG_URL', get_template_directory_uri() );
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/aaronholbrook/autoload/autoload.php';
 
 \AaronHolbrook\Autoload\autoload( __DIR__ . '/includes' );

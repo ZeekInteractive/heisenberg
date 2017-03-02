9a0d38faf04e412f9fec830f314b3fbebc22d383
Jacob Arriola <jacob@zeek.com>
2017-03-01 20:41:13 -0800

    Formatting

--- header.php
+++ header.php
@@ -11,12 +11,12 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -24,47 +24,44 @@
 <?php
 $svg_sprite = file_get_contents( get_template_directory() . '/assets/dist/sprite/sprite.svg' );
 if ( file_exists( $svg_sprite ) ) {
-    echo $svg_sprite;
+	echo $svg_sprite;
 } ?>
 
 <div class="off-canvas-wrapper">
-  <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-    <div class="title-bar show-for-small-only">
-      <div class="title-bar-left">
-        <button class="menu-icon" type="button" data-toggle="offCanvasLeft"></button>
-        <span class="title-bar-title"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
-      </div>
-    </div><!-- .title-bar -->
-    <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
-			<button class="close-button" aria-label="Close menu" type="button" data-close>
-				<span aria-hidden="true">&times;</span>
-			</button>
+    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+        <div class="title-bar show-for-small-only">
+            <div class="title-bar-left">
+                <button class="menu-icon" type="button" data-toggle="offCanvasLeft"></button>
+                <span class="title-bar-title"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
+            </div>
+        </div>
+        <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
+            <button class="close-button" aria-label="Close menu" type="button" data-close>
+                <span aria-hidden="true">&times;</span>
+            </button>
 			<?php
-			 $args = array (
-				 'theme_location' 	=> 'primary',
-				 'container' 				=> 'nav',
-				 'container_class'	=> 'offcanvas-navigation',
-				 'menu_class' 			=> 'mobile-menu',
-			 );
-				wp_nav_menu( $args );
-			?>
-    </div><!-- #offCanvasLeft -->
-    <div class="off-canvas-content" data-off-canvas-content>
-
-			<header id="masthead" class="" role="banner">
-				<section class="row column">
-					<h1 class="site-title">
-						<a href="<?php esc_attr_e( home_url( '/' ) ); ?>" rel="home">
+			$args = array(
+				'theme_location'  => 'primary',
+				'container'       => 'nav',
+				'container_class' => 'offcanvas-navigation',
+				'menu_class'      => 'mobile-menu',
+			);
+			wp_nav_menu( $args ); ?>
+        </div>
+        <div class="off-canvas-content" data-off-canvas-content>
+            <header id="masthead" class="" role="banner">
+                <section class="row column">
+                    <h1 class="site-title">
+                        <a href="<?php esc_attr_e( home_url( '/' ) ); ?>" rel="home">
 							<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-						</a>
-					</h1>
-					<h2 class="site-description"><?php echo esc_html( get_bloginfo( 'description' ) ); ?></h2>
-				</section>
-				<nav id="site-navigation" class="top-bar show-for-medium" data-topbar role="navigation">
-					<section class="top-bar-section row column">
+                        </a>
+                    </h1>
+                    <h2 class="site-description"><?php echo esc_html( get_bloginfo( 'description' ) ); ?></h2>
+                </section>
+                <nav id="site-navigation" class="top-bar show-for-medium" data-topbar role="navigation">
+                    <section class="top-bar-section row column">
 						<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-					</section>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
-
-			<div id="content" class="site-content">
+                    </section>
+                </nav>
+            </header>
+            <div id="content" class="site-content">

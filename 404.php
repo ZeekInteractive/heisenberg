3a51060bf677c0f8187b2be54956e4c8e740799a
Jacob Arriola <jacob@zeek.com>
2017-03-01 20:32:41 -0800

    Format markup

--- 404.php
+++ 404.php
@@ -17,44 +17,44 @@ get_header(); ?>
 
 			<main id="main" class="site-main" role="main">
 
-					<header class="page-header">
-						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'heisenberg' ); ?></h1>
-					</header>
+                <header class="page-header">
+                    <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'heisenberg' ); ?></h1>
+                </header>
 
-					<div class="page-content">
-						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'heisenberg' ); ?></p>
+                <div class="page-content">
+                    <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'heisenberg' ); ?></p>
 
-						<?php
-						get_search_form();
+                    <?php
+                    get_search_form();
 
-						the_widget( 'WP_Widget_Recent_Posts' );
+                    the_widget( 'WP_Widget_Recent_Posts' );
 
-						if ( heisenberg_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+                    if ( heisenberg_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 
-							<div class="widget widget_categories">
-								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'heisenberg' ); ?></h2>
-								<ul>
-								<?php
-									wp_list_categories( array(
-										'orderby'    => 'count',
-										'order'      => 'DESC',
-										'show_count' => 1,
-										'title_li'   => '',
-										'number'     => 10,
-									) );
-								?>
-								</ul>
-							</div><!-- .widget -->
+                        <div class="widget widget_categories">
+                            <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'heisenberg' ); ?></h2>
+                            <ul>
+                            <?php
+                                wp_list_categories( array(
+                                    'orderby'    => 'count',
+                                    'order'      => 'DESC',
+                                    'show_count' => 1,
+                                    'title_li'   => '',
+                                    'number'     => 10,
+                                ) );
+                            ?>
+                            </ul>
+                        </div><!-- .widget -->
 
-						<?php
-						endif;
+                    <?php
+                    endif;
 
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'heisenberg' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+                    $archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'heisenberg' ), convert_smilies( ':)' ) ) . '</p>';
+                    the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-						the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+                    the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-					</div><!-- .page-content -->
+                </div><!-- .page-content -->
 
 			</main><!-- #main -->
 

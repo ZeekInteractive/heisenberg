cc7997193bd74c983739ce05dd86d0e81e09344d
Aaron Holbrook <aaron@zeek.com>
2017-05-15 11:13:14 -0500

    Remove autoload files, use static loads

--- functions.php
+++ functions.php
@@ -9,6 +9,11 @@ define( 'HEISENBERG_VERSION', '2.0.5' );
 define( 'HEISENBERG_DIR', __DIR__ );
 define( 'HEISENBERG_URL', get_template_directory_uri() );
 
-require_once __DIR__ . '/vendor/aaronholbrook/autoload/autoload.php';
-
-\AaronHolbrook\Autoload\autoload( __DIR__ . '/includes' );
+require_once __DIR__ . '/src/custom-login.php';
+require_once __DIR__ . '/src/customizer.php';
+require_once __DIR__ . '/src/enqueue.php';
+require_once __DIR__ . '/src/extras.php';
+require_once __DIR__ . '/src/jetpack.php';
+require_once __DIR__ . '/src/setup.php';
+require_once __DIR__ . '/src/sidebars.php';
+require_once __DIR__ . '/src/template-tags.php';

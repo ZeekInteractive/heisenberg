00687853744d87c1dbe0c32751608cb3bc5e65b1
Jacob Arriola <jacob@zeek.com>
2017-08-28 11:54:46 -0700

    🔥 Remove explicity foundation js enqueue
    Everything is happening in the app.js file now.

--- src/enqueue.php
+++ src/enqueue.php
@@ -7,16 +7,7 @@ namespace Heisenberg;
  */
 add_action( 'wp_enqueue_scripts', function() {
 	$min_ext = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
-
-	// Add Foundation JS to footer
-	wp_enqueue_script(
-		'foundation-js',
-		HEISENBERG_URL . "/_dist/js/foundation{$min_ext}.js",
-		[ 'jquery' ],
-		'6.3.1',
-		true
-	);
-
+	
 	// Add our main app js file
 	wp_enqueue_script(
 		'heisenberg_appjs',

b2c9d674bfa241d9122312e03944d783e1cb9660
Jacob Arriola <jacob@zeek.com>
2018-10-12 15:40:28 -0700

    Remove unused CSS

--- sidebar.php
+++ sidebar.php
@@ -11,6 +11,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 } ?>
 
-<div id="secondary" class="widget-area" role="complementary">
+<div class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->

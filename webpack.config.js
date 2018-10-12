59175ebc6f7026474c3c1f216c983c0fb1a58055
Jacob Arriola <jacob@zeek.com>
2018-10-12 14:46:52 -0700

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -6,82 +6,82 @@ const autoprefixer = require('autoprefixer')
 
 let prefix = ''
 if (process.env.NODE_ENV === 'production') {
-  prefix = '.min'
+	prefix = '.min'
 }
 
 const css = {
-  loader: 'css-loader',
-  options: {
-    url: false,
-  }
+	loader: 'css-loader',
+	options: {
+		url: false,
+	}
 }
 
 const postcss = {
-  loader: 'postcss-loader',
-  options: {
-    sourceMap: true,
-    plugins () {
-      return [autoprefixer()]
-    }
-  }
+	loader: 'postcss-loader',
+	options: {
+		sourceMap: true,
+		plugins () {
+			return [autoprefixer()]
+		}
+	}
 }
 
 const sass = {
-  loader: 'sass-loader',
-  options: {
-    sourceMap: true,
-    outputStyle: 'compressed',
-  }
+	loader: 'sass-loader',
+	options: {
+		sourceMap: true,
+		outputStyle: 'compressed',
+	}
 }
 
 const config = {
-  entry: [
-    '@babel/polyfill',
-    './assets/js/main.js'
-  ],
-  output: {
-    filename: `build${prefix}.js`,
-    path: path.resolve(__dirname, 'dist')
-  },
-  mode: process.env.NODE_ENV,
-  externals: {
-    jquery: 'jQuery'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-	      options: {
-        	presets: ['@babel/preset-env']
-	      },
-        exclude: /node_modules\/(?!(foundation-sites)\/).*/ // https://github.com/zurb/foundation-sites/issues/10161#issuecomment-309503202
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, css, postcss, sass]
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve('assets')
-    }
-  },
-  plugins: [
-    new MiniCssExtractPlugin('[name].css'),
-    new BrowserSyncPlugin(
-      {
-        injectChanges: true,
-        proxy: process.env.LOCAL_URL || 'http://heisenberg.dev',
-        files: [
-          '**/*.php'
-        ],
-        port: process.env.PORT || 3000,
-        notify: false
-      }
-    )
-  ]
+	entry: [
+		'@babel/polyfill',
+		'./assets/js/main.js'
+	],
+	output: {
+		filename: `build${prefix}.js`,
+		path: path.resolve(__dirname, 'dist')
+	},
+	mode: process.env.NODE_ENV,
+	externals: {
+		jquery: 'jQuery'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				},
+				exclude: /node_modules\/(?!(foundation-sites)\/).*/ // https://github.com/zurb/foundation-sites/issues/10161#issuecomment-309503202
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, css, postcss, sass]
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve('assets')
+		}
+	},
+	plugins: [
+		new MiniCssExtractPlugin('[name].css'),
+		new BrowserSyncPlugin(
+			{
+				injectChanges: true,
+				proxy: process.env.LOCAL_URL || 'http://heisenberg.dev',
+				files: [
+					'**/*.php'
+				],
+				port: process.env.PORT || 3000,
+				notify: false
+			}
+		)
+	]
 }
 
 module.exports = config
